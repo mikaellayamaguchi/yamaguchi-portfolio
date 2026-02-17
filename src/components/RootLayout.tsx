b7676e44cce1de4aa68fb90a5ddd064f19173e14
mikaellayamaguchi <mikaellayamaguchi23@gmail.com>
2026-02-17 15:16:22 +0800

    feat: Add routing configuration and RootLayout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -3,7 +3,9 @@ import { Outlet } from "react-router";
 export const RootLayout = () => {
   return (
     <div className="min-h-screen bg-background">
-      {/* You can add a Navbar or Footer here later */}
+      {
+        
+      }
       <main>
         <Outlet /> 
       </main>
