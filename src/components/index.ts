464edacc7965633a1b36e9db1b7a19f608d24e41
mikaellayamaguchi <mikaellayamaguchi23@gmail.com>
2026-02-20 15:58:54 +0800

    feat: update index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export * from "./SEO";
+export * from "./TiltCard";
+export * from "./Navigation";
+export * from "./Footer";
+export * from "./RootLayout";
+export * from "./CustomCursor"; 
+// export * from "./Hero";
+// export * from "./AboutSection";
